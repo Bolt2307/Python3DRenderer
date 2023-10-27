42af05b56ab43a7fdd4f07a0c0899e95d2418fdb
Lrae <109258251+Lrae1207@users.noreply.github.com>
2023-10-27 13:28:12 -0700

    Added classes to render.py.
    I added it for easier reading.

--- render.py
+++ render.py
@@ -1,7 +1,41 @@
-from math import sin, cos, radians
+
+from math import sin, cos
+import tkinter
 import turtle
-vertices = [(-1, -1, -1), ( 1, -1, -1), ( 1,  1, -1), (-1,  1, -1),
-            (-1, -1,  1), ( 1, -1,  1), ( 1,  1,  1), (-1,  1,  1)]
+
+class Vector3: # Use this instead of triple tuples
+	x = 0
+	y = 0
+	z = 0
+	def Vector3(newX, newY, newZ):
+		x = newX
+		y = newY
+		z = newZ
+
+# Class definitions
+
+class Camera: # Represents physical attributes of a camera used to render
+	x = 0
+	y = 0
+	z = -5
+	xr = 0
+	yr = 0
+	zr = 0
+	fov = 400
+
+# Function definitions
+
+def anglesToVector(yaw,pitch): # Returns a Vector3 representing the direction as a 3d vector
+	x = cos(yaw) * cos(pitch)
+	z = sin(yaw) * cos(pitch)
+	y = sin(pitch)
+	return Vector3(x,y,z)
+
+def rotate(x, y, r): # PLEASE DOCUMENt THIS MAX
+  return x * cos(r) - y * sin(r), x * sin(r) + y * cos(r)
+
+# Replace this with Vector3-composed objects
+vertices = [(-1, -1, -1), ( 1, -1, -1), ( 1,  1, -1), (-1,  1, -1), (-1, -1,  1), ( 1, -1,  1), ( 1,  1,  1), (-1,  1,  1)]
 triangles = [
     (0, 1, 2), (2, 3, 0),
     (0, 4, 5), (5, 1, 0),
@@ -10,14 +44,8 @@ triangles = [
     (7, 6, 3), (6, 2, 3),
     (5, 1, 2), (2, 6, 5)]
 
-class cam:
-	x = -5
-	y = 0
-	z = -5
-	xr = 0
-	yr = 45
-	zr = 0
-	fov = 400
+# Example of class instantiation
+cam = Camera()
 
 window = turtle.Screen()
 window.title("YEAH BABY!")
@@ -26,10 +54,7 @@ pointer.hideturtle()
 turtle.tracer(0, 0)
 pointer.up()
 
-def rotate(x, y, r):
-  return x * cos(r) - y * sin(r), x * sin(r) + y * cos(r)
-
-counter = 0
+# Main loop
 while True:
 	pointer.clear()
 	for triangle in triangles:
@@ -37,7 +62,7 @@ while True:
 		for vertex in triangle:
 			x, y, z = vertices[vertex]
 			x, y, z = x - cam.x, y - cam.y, z - cam.z
-			xr, yr, zr = radians(cam.xr), radians(cam.yr), radians(cam.zr)
+			xr, yr, zr = -cam.xr, -cam.yr, -cam.zr
 			x, z = rotate(x, z, yr)
 			y, z = rotate(y, z, xr)
 			x, y = rotate(x, y, zr)
