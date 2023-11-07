26f343ae8b0ff28be2bd05b4ac007a0b7011291a
Lrae <109258251+Lrae1207@users.noreply.github.com>
2023-11-06 17:09:26 -0800

    A few non-functional changes to render.py

--- render.py
+++ render.py
@@ -37,6 +37,14 @@ class Face:
         self.col = color
         self.connection_vertices = vertices
 
+class BoxCollider: # I will work on polygon colliders later
+    isVisible = True
+    vertices = ((0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0))
+
+    def set_collider(self,new_vertices):
+        for i in range(new_vertices):
+            self.vertices[i] = new_vertices[i]
+
 class Object:
     position = Vector3(0,0,0)
 
@@ -45,6 +53,10 @@ class Object:
 
     renderable = True
     visible = True
+
+    collider = BoxCollider()
+
+    on_collision = None # execute this function on a collision with another object
 
     vertices = [] # List of all points as tuples 
     points = [] # List of all points as points
@@ -61,7 +73,7 @@ speed = 0.025
 
 init()
 font.init()
-analytics_font = font.SysFont('cousine', 15)
+analytics_font = font.SysFont('cousine', 20)
 screen = display.set_mode((0,0), FULLSCREEN)
 running = True
 display.set_caption('YEAH BABY!')
@@ -101,6 +113,8 @@ for v in f:
 
 
 objects.append(cube)
+cube.collider.set_collider([(-1, -1, -1), ( 1, -1, -1), ( 1,  1, -1), (-1,  1, -1), (-1, -1,  1), ( 1, -1,  1), ( 1,  1,  1), (-1,  1,  1)])
+
 
 def rotate_point(x, y, r):
   return x * cos(r) - y * sin(r), x * sin(r) + y * cos(r)
@@ -109,7 +123,7 @@ def zsort (input):
     return input[0][0][2]
 
 def render ():
-    t0 = time.process_time_ns()
+    t0 = time.perf_counter_ns()
     for obj in objects:
         for face in obj.points:
             show = False
@@ -127,10 +141,10 @@ def render ():
                 points.append((x * cam.focal_length/z+scrn.width/2, -y * cam.focal_length/z+scrn.height/2)) #vector3 coord
             if show == True:
                 draw.polygon(screen, RGBColor.to_tuple(face.col), points, obj.wire_thickness) #shape
-    return time.process_time_ns()-t0
+    return time.perf_counter_ns()-t0
 
 def gui ():
-    t0 = time.process_time_ns()
+    t0 = time.perf_counter_ns()
     global crosshairspread
     crosshairspread = speed * 100
     
@@ -139,10 +153,10 @@ def gui ():
     draw.line(screen, 'red', (scrn.width/2+crosshairspread, scrn.height/2), (scrn.width/2+10+crosshairspread, scrn.height/2)) #horizontal right
     draw.line(screen, 'red', (scrn.width/2, scrn.height/2-10-crosshairspread), (scrn.width/2, scrn.height/2-crosshairspread)) #vertical top
     draw.line(screen, 'red', (scrn.width/2, scrn.height/2+crosshairspread), (scrn.width/2, scrn.height/2+10+crosshairspread)) #vertical vertical bottom
-    return time.process_time_ns()-t0
+    return time.perf_counter_ns()-t0
 
 def handle_control ():
-    t0 = time.process_time_ns()
+    t0 = time.perf_counter_ns()
     global pausecooldown
     global pause
     global speed
@@ -179,7 +193,7 @@ def handle_control ():
             cam.velocity.x += speed*sin(radians(cam.rotation.x+90))
         if keys[K_SPACE]:
             if cam.position.y <= 0:
-                cam.velocity.y += 1
+                cam.velocity.y += 0.5
         if keys[K_LCTRL]:
             if cam.height > -1:
                 cam.height -= 0.2
@@ -205,80 +219,105 @@ def handle_control ():
                 mouse.set_cursor(SYSTEM_CURSOR_CROSSHAIR)
     else:
         pausecooldown -= 0.1 #makes sure that pauseape is not held and spammed
-    return time.process_time_ns()-t0
+    return time.perf_counter_ns()-t0
 
 bgcolor = (random.randrange(0,255), random.randrange(0,255), random.randrange(0,255))
 cam.position.z = -10
 
 def update():
-    t0 = time.process_time_ns()
+    t0 = time.perf_counter_ns()
     if pause == False:
         mouse.set_pos(scrn.width/2, scrn.height/2) #mouse "lock"
 	    # Change position by velocity and apply drag to velocity
         cam.position.x, cam.position.y, cam.position.z = cam.position.x + cam.velocity.x, cam.position.y + cam.velocity.y, cam.position.z + cam.velocity.z
         cam.velocity.x, cam.velocity.y, cam.velocity.z = cam.velocity.x * 0.85, cam.velocity.y * 0.85, cam.velocity.z * 0.85
         if cam.position.y > 0: # Apply Gravity
-            cam.velocity.y -= 0.05
+            cam.velocity.y -= 0.02
         else:
             cam.velocity.y = 0
             cam.position.y = 0
-    return time.process_time_ns()-t0
+    return time.perf_counter_ns()-t0
 
-def print_elapsed_time(cntrl_time, engine_update_time, render_time_3D, render_time_2D):
-    cntrl_text = analytics_font.render('control_update: ' + str(cntrl_time / 1000) + ' us', False, (0, 0, 0))
-    engine_update_text = analytics_font.render('engine_update: ' + str(engine_update_time / 1000) + ' us', False, (0, 0, 0))
-    render_3D_text = analytics_font.render('render_time_3D: ' + str(render_time_3D / 1000) + ' us', False, (0, 0, 0))
-    render_2D_text = analytics_font.render('render_time_2D: ' + str(render_time_2D / 1000) + ' us', False, (0, 0, 0))
-    total_text = analytics_font.render('total: ' + str((render_time_2D + cntrl_time + engine_update_time + render_time_3D) / 1000000) + ' ms', False, (0, 0, 0))
-    screen.blit(cntrl_text, (0,0))
-    screen.blit(engine_update_text, (0,20))
-    screen.blit(render_3D_text, (0,40))
-    screen.blit(render_2D_text, (0,60))
-    screen.blit(total_text, (0,80))
+# Prints data and debug information to view while running
+def print_elapsed_time(cntrl_time, engine_update_time, render_time_3D, render_time_2D, idle_time, fps):
+    cntrl_text = analytics_font.render('control_update: ' + str(round(cntrl_time / 1000,2)) + ' us', False, (0, 0, 0))
+    engine_update_text = analytics_font.render('engine_update: ' + str(round(engine_update_time / 1000,2)) + ' us', False, (0, 0, 0))
+    render_3D_text = analytics_font.render('render_time_3D: ' + str(round(render_time_3D / 1000,2)) + ' us', False, (0, 0, 0))
+    render_2D_text = analytics_font.render('render_time_2D: ' + str(round(render_time_2D / 1000,2)) + ' us', False, (0, 0, 0))
+    total_text = analytics_font.render('total: ' + str(round((render_time_2D + cntrl_time + engine_update_time + render_time_3D) / 1000000,2)) + ' ms', False, (0, 0, 0))
+    idle_text = analytics_font.render('idle_time: ' + str(round(idle_time * 1000,2)) + ' ms', False, (0, 0, 0))
+    fps_text = analytics_font.render('fps: ' + str(round(fps)), False, (0, 0, 0))
+    screen.blit(cntrl_text, (5,0))
+    screen.blit(engine_update_text, (5,20))
+    screen.blit(render_3D_text, (5,40))
+    screen.blit(render_2D_text, (5,60))
+    screen.blit(total_text, (5,80))
+    screen.blit(idle_text, (5,100))
+    screen.blit(fps_text, (5,120))
 
+# Timing/frame_cap variables
 frame = 0
-cntrl_time = 0
-engine_update_time = 0
-render_time_3D = 0
-render_time_2D = 0
-time_elapsed = 0
+frame_cap = 120
+
+# Measurement variables
+cntrl_time = 0 # Nanoseconds
+engine_update_time = 0 # Nanoseconds
+render_time_3D = 0  # Nanoseconds
+render_time_2D = 0 # Nanoseconds
+time_elapsed = 0 # Seconds
+measured_fps = 0
+idle_time = 0 # Seconds
+
+last_timestamp = time.perf_counter() # Seconds
+
 while running:
-    frame += 1
-    # Handle events
-    for gevent in event.get():
-        if gevent.type == QUIT:
-            running = False
-    # Background color
-    screen.fill(bgcolor)
-    
-    # Print elapsed time ever n frames
-    if frame % 60 == 0:
-        # Take user input
-        cntrl_time = handle_control()
+    current_timestamp = time.perf_counter()
+    time_elapsed += current_timestamp - last_timestamp # Add change in time to the time_elapsed
+    last_timestamp = current_timestamp
+    if time_elapsed > 1 / frame_cap: # Do not update unless enough time has passed
+        frame += 1
 
-        # Update
-        engine_update_time = update()
+        # Handle events
+        for gevent in event.get():
+            if gevent.type == QUIT:
+                running = False
+        # Background color
+        screen.fill(bgcolor)
         
-        # Render objects
-        render_time_3D = render()
+        # Print elapsed time ever n frames
+        if frame % 60 == 0:
+            # Take user input
+            cntrl_time = handle_control()
 
-        # Render GUI
-        render_time_2D = gui()
-        print_elapsed_time(cntrl_time, engine_update_time, render_time_3D, render_time_2D)
-    else:
-        # Take user input
-        handle_control()
+            # Update
+            engine_update_time = update()
+            
+            # Render objects
+            render_time_3D = render()
 
-        # Update
-        update()
-        
-        # Render objects
-        render()
+            # Render GUI
+            render_time_2D = gui()
 
-        # Render GUI
-        gui()
-        print_elapsed_time(cntrl_time, engine_update_time, render_time_3D, render_time_2D)
+            # Update measurement variables
+            measured_fps = 1/time_elapsed
+            idle_time = time_elapsed
 
-    display.flip() # Invert screen
-    display.update() # Display new render
+            print_elapsed_time(cntrl_time, engine_update_time, render_time_3D, render_time_2D, time_elapsed, measured_fps)
+        else:
+            # Take user input
+            handle_control()
+
+            # Update
+            update()
+            
+            # Render objects
+            render()
+
+            # Render GUI
+            gui()
+            print_elapsed_time(cntrl_time, engine_update_time, render_time_3D, render_time_2D, idle_time, measured_fps)
+
+        display.flip() # Invert screen
+        display.update() # Display new render
+        time_elapsed = 0 # Reset elapsed time
 quit()
