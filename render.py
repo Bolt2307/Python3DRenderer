56adbef0dfbbd2cd4b40ab726cedf5a88209fae6
Bolt2307 <94214866+Bolt2307@users.noreply.github.com>
2023-11-27 13:08:31 -0800

    Update render.py

--- render.py
+++ render.py
@@ -2,14 +2,11 @@ import pygame
 import math
 import time
 import json
-
-# !IMPORTANT!
-scene_path = "scenepath.json"
-# Change this path ^ to the current path of the "scene.json" file on your system
+import os
 
 # Class definitions
 
-# Triple tuple representing 3d oordinates, 3d rotation, 3d movement, etc.
+# Triple tuple representing 3d coordinates, 3d rotation, 3d movement, etc.
 class Vector3:
     x, y, z = 0, 0, 0
 
@@ -68,7 +65,8 @@ class Face:
         self.color = color
 
 class Object:
-    isStatic = True
+    static = True
+    locked = False
 
     id = ""
     position = Vector3(0,0,0)
@@ -85,7 +83,7 @@ class Object:
     vertices = [] # List of all points as Vector3 
     faces = [] # List of all faces as faces
 
-    def __init__ (self, id, position, orientation, origin, scale, wire_thickness, visible, transparent, vertices, faces):
+    def __init__ (self, id, position, orientation, origin, scale, wire_thickness, visible, transparent, static, vertices, faces):
         self.id = id
         self.position = position
         self.orientation = orientation
@@ -94,6 +92,7 @@ class Object:
         self.wire_thickness = wire_thickness
         self.visible = visible
         self.transparent = transparent
+        self.static = static
         self.vertices = vertices
         self.faces = faces
 
@@ -101,20 +100,32 @@ class Object:
         for face in self.faces:
             face.color = color
 
-def find_obj (id, list = None):
-    if list == None:
-        list = objects
-    for obj in list:
-        if obj.id == id:
-            return list.index(obj)
+def rel_dir (str):
+    if (str[0] == "/") | (str[0] == "."):
+        return str
+    else:
+        return os.path.dirname(__file__) + "/" + str
+class find_obj:
+    def by_id (id, list = None):
+        if list == None:
+            list = objects
+        for obj in list:
+            if obj.id == id:
+                return list.index(obj)
+    def by_position (pos, list = None, skip = []):
+        if list == None:
+            list = objects
+        for obj in list:
+            if (obj.position.to_tuple() == pos.to_tuple()) & (obj.id not in skip):
+                return list.index(obj)
 
 def copy_obj (id, new_id, list = None, new_list = None):
     if list == None:
         list = objects
     if new_list == None:
         new_list = list
-    obj = list[find_obj(id)]
-    new_list.append(Object(new_id, obj.position, obj.orientation, obj.origin, obj.scale, obj.wire_thickness, obj.visible, obj.transparent, obj.vertices, obj.faces))
+    obj = list[find_obj.by_id(id)]
+    new_list.append(Object(new_id, obj.position, obj.orientation, obj.origin, obj.scale, obj.wire_thickness, obj.visible, obj.transparent, obj.static, obj.vertices, obj.faces))
 
 def rotate_point (x, y, r):
   return x * math.cos(r) - y * math.sin(r), x * math.sin(r) + y * math.cos(r)
@@ -130,31 +141,43 @@ def shoelace (pts):
     except:
         return 0
 
-def render ():
+def render (window, list):
+    global objectsnum
+    global facesnum
+    screen.fill(bgcolor)
     t0 = time.perf_counter_ns()
     zbuffer = []
 
-    for obj in objects:
-        for face in obj.faces:
-            if obj.visible == True:
+    for obj in list:
+        if obj.visible:
+            objectsnum += 1
+            locked = obj.locked
+            for face in obj.faces:
                 show = True # The object will be rendered unless one of its vertices is out-of-scope
                 points = []
                 depthval = 0
                 for vertex in face.indices:
-                    # Scaling
-                    x = vertex.x * obj.scale.x
-                    y = vertex.y * obj.scale.y
-                    z = vertex.z * obj.scale.z
+                    x, y, z = vertex.x, vertex.y, vertex.z
+                    if not locked:
+                        # Scaling
+                        x *= obj.scale.x
+                        y *= obj.scale.y
+                        z *= obj.scale.z
 
-                    # Rotation
-                    x, z = rotate_point(x - obj.origin.x, z - obj.origin.z, math.radians(obj.orientation.y))
-                    y, z = rotate_point(y - obj.origin.y, z - obj.origin.z, math.radians(obj.orientation.x))
-                    x, y = rotate_point(x - obj.origin.x, y - obj.origin.y, math.radians(obj.orientation.z))
+                        # Rotation
+                        x, z = rotate_point(x - obj.origin.x, z - obj.origin.z, math.radians(obj.orientation.y))
+                        y, z = rotate_point(y - obj.origin.y, z - obj.origin.z, math.radians(obj.orientation.x))
+                        x, y = rotate_point(x - obj.origin.x, y - obj.origin.y, math.radians(obj.orientation.z))
+                        
+                        # Offset
+                        x += obj.position.x
+                        y += obj.position.y
+                        z += obj.position.z
 
-                    # Offset
-                    x += obj.position.x
-                    y += obj.position.y
-                    z += obj.position.z
+                        # Lock Vertices For Static Objects
+                        if obj.static:
+                            face.indices[face.indices.index(vertex)] = Vector3(x, y, z)
+                            obj.locked = True
 
                     # Rotation relative to camera
                     x, y, z = x - cam.position.x, y - cam.position.y - cam.height, z - cam.position.z
@@ -166,8 +189,11 @@ def render ():
                     if z < 0: # Do not render clipping or out-of-scope objects
                         show = False
                         break
-                        
-                    points.append(((x * cam.focal_length/z+screen.get_width()/2) * (screen.get_width() / Screen.fullwidth), (-y * cam.focal_length/z+screen.get_height()/2)*(screen.get_height() / Screen.fullheight)))
+
+                    try:
+                        points.append(((x * cam.focal_length/z+screen.get_width()/2) * (screen.get_width() / Screen.fullwidth), (-y * cam.focal_length/z+screen.get_height()/2)*(screen.get_height() / Screen.fullheight)))
+                    except:
+                        pass
                     depthval += z # add z to the sum of the z values
 
                 depthval /= len(face.indices) # depthval now stores the z of the object's center
@@ -175,7 +201,8 @@ def render ():
                     zbuffer.append([face.color.to_tuple(), points, obj.wire_thickness, depthval]) # Store the info in zbuffer
     zbuffer.sort(key=lambda x: x[3], reverse=True) # Sort z buffer by the z distance from the camera
     for face in zbuffer: # Draw each face
-        pygame.draw.polygon(screen, face[0], face[1], face[2])
+        pygame.draw.polygon(window, face[0], face[1], face[2])
+        facesnum += 1
 
     return time.perf_counter_ns() - t0
                 
@@ -191,27 +218,30 @@ def gui ():
     pygame.draw.line(screen, 'red', (screen.get_width()/2+crosshairspread, screen.get_height()/2), (screen.get_width()/2+10+crosshairspread, screen.get_height()/2)) #horizontal right
     pygame.draw.line(screen, 'red', (screen.get_width()/2, screen.get_height()/2-10-crosshairspread), (screen.get_width()/2, screen.get_height()/2-crosshairspread)) #vertical top
     pygame.draw.line(screen, 'red', (screen.get_width()/2, screen.get_height()/2+crosshairspread), (screen.get_width()/2, screen.get_height()/2+10+crosshairspread)) #vertical vertical bottom
-    if pause == True:
+
+    if pause: # Show pause menu
         pausetext = analytics_font.render('PAUSED', False, (200, 0, 0))
         screen.blit(pausetext, (screen.get_width()/2, 0))
-    if specstog == True:
+    
+    if specstog: # Show spects
             print_elapsed_time(cntrl_time, engine_update_time, render_time_3D, render_time_2D, idle_time, measured_fps)
     return time.perf_counter_ns()-t0
 
-def handle_control ():
+def handle_control():
     t0 = time.perf_counter_ns()
-    global pausecooldown
     global specstog
+    global specsHeld
     global pause
+    global pauseHeld
     global speed
     global running
-    global place
-    placecooldown = 0
+    global cubenum
     keys = pygame.key.get_pressed()
 
     #rotation
     rel = pygame.mouse.get_rel()
-    if pause == False:
+
+    if not pause: # When unpaused
         cam.rotation.y += rel[0]*0.15
         cam.rotation.x -= rel[1]*0.15 #mouse sense
 
@@ -238,6 +268,16 @@ def handle_control ():
         if keys[pygame.K_d]:
             cam.velocity.z += speed*math.cos(math.radians(cam.rotation.y+90))
             cam.velocity.x += speed*math.sin(math.radians(cam.rotation.y+90))
+        if keys[pygame.K_e]:
+            copy_obj("cube", "cube" + str(cubenum))
+            objects[find_obj.by_id("cube" + str(cubenum))].position = objects[find_obj.by_id("wire_cube")].position
+            objects[find_obj.by_id("cube" + str(cubenum))].visible = True
+            cubenum += 1
+        if keys[pygame.K_q]:
+            try:
+                del objects[find_obj.by_position(objects[find_obj.by_id("wire_cube")].position, objects, ["wire_cube"])]
+            except:
+                pass
         if keys[pygame.K_SPACE]:
             if cam.position.y <= 0:
                 cam.velocity.y += 1
@@ -247,56 +287,45 @@ def handle_control ():
         else:
             if cam.height < 0:
                 cam.height += 0.2
-        if placecooldown <= 0:
-            if keys[pygame.K_q] & (place == False):
-                place = True
-                placecooldown = 0.1
-            else:
-                place = False
-        else:
-            placecooldown -= 0.01
-
-    #misc
-    else:
+    else: # In pause menu
         if keys[pygame.K_e]: #exits the game if e is pressed in pause
             running = False
+
         if keys[pygame.K_f]:
-            if specstog == True:
-                specstog = False
-            elif specstog == False:
-                specstog = True
-    if pausecooldown < 0: #pauses the game on escape
-        if keys[pygame.K_ESCAPE]:
-            pausecooldown = 0.2
-            if pause == False:
-                pause = True
-                pygame.mouse.set_visible(True)
-                pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
-            elif pause == True:
-                pause = False
-                pygame.mouse.set_visible(False)
-                pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_CROSSHAIR)
+            if not specsHeld:
+                specstog = not specstog
+                specsHeld = True
+        else:
+            specsHeld = False
+
+    if keys[pygame.K_ESCAPE]:
+        if (not pause) & (not pauseHeld): # Pause handling
+            pauseHeld = True
+            pause = True
+            pygame.mouse.set_visible(True)
+            pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
+        elif pause & (not pauseHeld):
+            pauseHeld = True
+            pause = False
+            pygame.mouse.set_visible(False)
+            pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_CROSSHAIR)
     else:
-        pausecooldown -= 0.02 #makes sure that pause key is not held and spammed
+        pauseHeld = False
+
     return time.perf_counter_ns() - t0
 
 def update():
     global tick
-    global cubenum
     t0 = time.perf_counter_ns()
-    if pause == False:
+    if not pause:
         pygame.mouse.set_pos(screen.get_width()/2, screen.get_height()/2) #mouse "lock"
 
-        if place:
-            x, y, z, = 0, 0, 2
-            y, z = rotate_point(y, z, -math.radians(cam.rotation.x))
-            x, z = rotate_point(x, z, -math.radians(cam.rotation.y))
-            x, y = rotate_point(x, y, -math.radians(cam.rotation.z))
-            x, y, z = round((x + cam.position.x) / 0.5) * 0.5, round((y + cam.position.y) / 0.5) * 0.5, round((z + cam.position.z) / 0.5) * 0.5
-            copy_obj("cube", "cube" + str(cubenum))
-            objects[find_obj("cube" + str(cubenum))].position = Vector3(x, y, z)
-            objects[find_obj("cube" + str(cubenum))].visible = True
-            cubenum += 1
+        x, y, z, = 0, 0, 2
+        y, z = rotate_point(y, z, -math.radians(cam.rotation.x))
+        x, z = rotate_point(x, z, -math.radians(cam.rotation.y))
+        x, y = rotate_point(x, y, -math.radians(cam.rotation.z))
+        x, y, z = round((x + cam.position.x) / 0.5) * 0.5, round((y + cam.position.y) / 0.5) * 0.5, round((z + cam.position.z) / 0.5) * 0.5
+        objects[find_obj.by_id("wire_cube")].position = Vector3(x, y, z)
 
 	    # Change position by velocity and apply drag to velocity
         cam.position.x, cam.position.y, cam.position.z = cam.position.x + cam.velocity.x, cam.position.y + cam.velocity.y, cam.position.z + cam.velocity.z
@@ -319,6 +348,8 @@ def print_elapsed_time(cntrl_time, engine_update_time, render_time_3D, render_ti
     idle_text = analytics_font.render('idle_time: ' + str(round(idle_time * 1000,2)) + ' ms', False, (0, 0, 0))
     fps_text = analytics_font.render('fps: ' + str(round(fps)), False, (0, 0, 0))
     tick_text = analytics_font.render('tick: ' + str(tick), False, (0, 0, 0))
+    facesnum_text = analytics_font.render('faces being rendered: ' + str(facesnum), False, (0, 0, 0))
+    objectsnum_text = analytics_font.render('objects being rendered: ' + str(objectsnum), False, (0, 0, 0))
     screen.blit(cntrl_text, (5,0))
     screen.blit(engine_update_text, (5,20))
     screen.blit(render_3D_text, (5,40))
@@ -327,14 +358,16 @@ def print_elapsed_time(cntrl_time, engine_update_time, render_time_3D, render_ti
     screen.blit(idle_text, (5,100))
     screen.blit(fps_text, (5,120))
     screen.blit(tick_text, (5, 140))
+    screen.blit(facesnum_text, (5,160))
+    screen.blit(objectsnum_text, (5, 180))
 
 # Main section
 
-cubenum = 0
-place = False
+bgcolor = (255, 255, 255)
 specstog = False
+specsHeld = False
 pause = False
-pausecooldown = 0
+pauseHeld = False
 crosshairspread = 0
 speed = 0.025
 
@@ -353,36 +386,40 @@ pygame.mouse.set_visible(False)
 # Instantiate classes
 cam = Camera()
 
-# Initiate Object Lists
-objects = []
-
 # Timing/frame_cap variables
 frame = 0
 frame_cap = 1000
 
 # Load scene JSON as objects
-with open(scene_path) as file:
-    scene = json.load(file)
-    bgcolor = tuple(scene["bg_color"])
-file.close()
-for objpath in scene["object_file_paths"]:
-    with open(scene["folder_path"] + objpath) as file:
-        obj = json.load(file)
-        vertices = []
-        for vertex in obj["vertices"]:
-            vertices.append(Vector3(tuple(vertex)))
-        faces = []
-        for face in obj["faces"]:
-            faces.append(Face((tuple(face[0])), RGBColor(tuple(face[1]))))
-        objects.append(Object(obj["id"], Vector3(tuple(obj["position"])), Vector3(tuple(obj["orientation"])), Vector3(tuple(obj["origin"])), Vector3(tuple(obj["scale"])), obj["wire_thickness"], obj["visible"], obj["transparent"], vertices, faces))
+def load_objects (path):
+    objlist = []
+    with open(path) as file:
+        scene = json.load(file)
+        bgcolor = tuple(scene["bg_color"])
+        folder_path = rel_dir(scene["folder_path"])
     file.close()
+    for objpath in scene["object_file_paths"]:
+        with open(folder_path + objpath) as file:
+            obj = json.load(file)
+            vertices = []
+            for vertex in obj["vertices"]:
+                vertices.append(Vector3(tuple(vertex)))
+            faces = []
+            for face in obj["faces"]:
+                faces.append(Face((tuple(face[0])), RGBColor(tuple(face[1]))))
+            objlist.append(Object(obj["name"], Vector3(tuple(obj["position"])), Vector3(tuple(obj["orientation"])), Vector3(tuple(obj["origin"])), Vector3(tuple(obj["scale"])), obj["wire_thickness"], obj["visible"], obj["transparent"], obj["static"], vertices, faces))
+        file.close()
+    return objlist
 
 # Precompiling faces
-for obj in objects:
-    for face in obj.faces:
-        face.indices = list(face.indices)
-        for index in range(len(face.indices)):
-            face.indices[index] = obj.vertices[face.indices[index]]
+def compile (objects):
+    objlist = objects
+    for obj in objlist:
+        for face in obj.faces:
+            face.indices = list(face.indices)
+            for index in range(len(face.indices)):
+                face.indices[index] = obj.vertices[face.indices[index]]
+    return objlist
 
 # Measurement variables
 cntrl_time = 0 # Nanoseconds
@@ -393,8 +430,11 @@ time_elapsed = 0 # Seconds
 measured_fps = 0
 idle_time = 0 # Seconds
 tick = 0 #ticks up 1 after every update
+cubenum = 0
 
 last_timestamp = time.perf_counter() # Seconds
+
+objects = compile(load_objects(rel_dir("scenepath.json")))
 
 while running:
     current_timestamp = time.perf_counter()
@@ -406,8 +446,6 @@ while running:
         # Handle events
         for gevent in pygame.event.get():
             pass
-        # Background color
-        screen.fill(bgcolor)
         
         # Print elapsed time ever n frames
         if frame % 60 == 0:
@@ -418,7 +456,9 @@ while running:
             engine_update_time = update()
             
             # Render objects
-            render_time_3D = render()
+            objectsnum = 0
+            facesnum = 0
+            render_time_3D = render(screen, objects)
 
             # Render GUI
             render_time_2D = gui()
@@ -435,7 +475,9 @@ while running:
             update()
             
             # Render objects
-            render()
+            objectsnum = 0
+            facesnum = 0
+            render(screen, objects)
 
             # Render GUI
             gui()
